4087467c6e12b1d6f7fb7f4a426ba88dece5f13e
driden <776982+driden@users.noreply.github.com>
2018-04-05 23:25:30 -0300

    done

--- Entrega1/Lab1.hs
+++ Entrega1/Lab1.hs
@@ -1,15 +1,32 @@
+-- Agustin Guerra - Luis Loureiro
+-- 196344 - 191659
 module Lab1 where
+
     import Prelude
     
     type Var = String
     
     --EJERCICIO 1.1--
     
-    data BinCon =  And| Or | Impl
-        deriving (Show, Eq)
-    data Form = L Var | Not Form | Bc BinCon Form Form
-        deriving (Show, Eq)
-       
+    -- 1.1.1
+    
+    data BinCon = And | Or | Impl | Eq deriving (Eq, Show)
+    
+    
+    data Form = L Var |
+                Not Form | 
+                Bc BinCon Form Form deriving (Eq, Show)
+    
+    -- Constantes para Debug
+    p :: Form
+    p = Not ( Bc And (L "p") ( Not (Not(L "p"))))
+    
+    q :: Form
+    q = Bc Impl (L "q") (L "q")
+    
+    s :: Form
+    s = Not (Bc And (L "s") (Not (Bc Or (L "s") (L "k") )))
+    -- 1.1.2
     --EJERCICIO 1.2--
     --a)
     uno :: Form
@@ -31,8 +48,6 @@ module Lab1 where
     }
     valores (Bc bin s t) = valores s ++ valores t 
     
-    {-  data BinCon =  And| Or | Impl
-        data Form = L Var | Not Form | Bc BinCon Form Form-}
     --2.2--
     cantNeg :: Form -> Int
     cantNeg (L q) = 0
@@ -73,7 +88,7 @@ module Lab1 where
     sustCon c1 c2 (Not q)      = Not (sustCon c1 c2 q)
     sustCon c1 c2 (Bc bin q r) = if bin == c1 then Bc c2 (sustCon c1 c2 q) (sustCon c1 c2 r) 
                                  else Bc bin (sustCon c1 c2 q) (sustCon c1 c2 r)
-
+    
     --2.8--
     swapCon :: BinCon -> BinCon -> Form -> Form
     swapCon c1 c2 (L q)        = L q
@@ -85,26 +100,55 @@ module Lab1 where
                                 else 
                                     Bc bin (swapCon c1 c2 q) (swapCon c1 c2 r)
     
-{-
     --2.9--
     dobleNeg :: Form -> Form
-    dobleNeg = undefined
+    dobleNeg = \e -> case e of {
+        L p -> L p;
+        Bc bin t s -> Bc bin (dobleNeg(t)) (dobleNeg(s));
+        Not t -> case t of {
+            L p -> Not (L p);
+            Bc binn l m -> Not (Bc binn (dobleNeg(l)) (dobleNeg(m)));
+            Not q -> dobleNeg(q);
+        }
+    }
     
     --2.10--
     sustSimp :: Var -> Form -> Form -> Form
-    sustSimp = undefined
+    sustSimp = \e m n -> case n of {
+        L k -> if k==e then m else L k;    
+        Not p -> Not (sustSimp e m p);
+        Bc bin t r -> Bc bin (sustSimp e m t) (sustSimp e m r);
+    }
+    
+    
+    buscarOcurrencia :: [(Var, Form)] -> Var -> (Form,Bool)
+    buscarOcurrencia = \lista variable -> case lista of {
+        [] -> (L "q",False);
+        (v,f):xs -> if v==variable then (f,True) else buscarOcurrencia xs variable
+    }
+    
+    arr :: [(Var, Form)]
+    arr = [("q",Not (L "q")),("p",Not (L "p")),("t",Not (L "t")),("r",Not (L "r"))]
     
     --2.11--
     sustMult :: [(Var, Form)] -> Form -> Form
-    sustMult = undefined
-    -}
-
+    sustMult = \lista formula  -> case formula of {
+        L p -> case buscarOcurrencia lista p of {
+            (r,True) ->    r;
+            (m,False) ->  L p;
+        };
+        Not q -> Not (sustMult lista q);
+        Bc bin z x -> Bc bin (sustMult lista z) (sustMult lista x);
+    }
+    
+    --Auxiliares--
+    
     inArray :: Var -> [Var] -> Bool
     inArray e arr = case arr of {
         [] -> False;
         x:xs -> (e == x) || inArray e xs;
     }
-
+    
     mergeArray :: [Var] -> [Var] -> [Var]
     mergeArray [] ys = ys
     mergeArray xs [] = xs
